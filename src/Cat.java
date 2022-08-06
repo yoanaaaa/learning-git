776fc2f9431e92bc14773478a81f4d36f9def8e4
Yoana Slavcheva <Yoana.Slavcheva@mail.schwarz>
2022-08-06 19:40:00 +0300

    Adding new class Cat and interface AnimalsI

---
+++ src/Cat.java
@@ -0,0 +1,17 @@
+public class Cat extends Animals {
+
+    @Override
+    public String makeNoise() {
+        return null;
+    }
+
+    @Override
+    public void GetName() {
+
+    }
+
+    @Override
+    public int numberOfLegs() {
+        return 0;
+    }
+}
