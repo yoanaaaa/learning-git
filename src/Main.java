f92096ad96d34be8430a275992e5d8f02536290a
Yoana Slavcheva <Yoana.Slavcheva@mail.schwarz>
2022-08-07 19:59:32 +0300

    added setVegetarian

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Cat tom = new Cat();
         tom.setNoOfLegs(4);
+        tom.setVegetarian(false);
         tom.setName("Tommy");
 
     }
