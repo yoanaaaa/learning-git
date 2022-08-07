654b393c1e75496aba119cebdcb02c910192d6f3
Yoana Slavcheva <Yoana.Slavcheva@mail.schwarz>
2022-08-07 23:33:52 +0300

    resolved

--- src/AnimalsI.java
+++ src/AnimalsI.java
@@ -11,7 +11,3 @@ interface  AnimalsI {
 
 }
 
-
-
-
-
---
+++ src/Frog.java
@@ -0,0 +1,44 @@
+public class Frog implements AnimalsI {
+    @Override
+    public String makeNoise() {
+
+        return "yes";
+
+    }
+
+    @Override
+    public void GetName() {
+
+    }
+
+    @Override
+    public boolean isVegetarian() {
+        return false;
+    }
+
+    @Override
+    public int numberOfLegs() {
+        return 0;
+    }
+
+    @Override
+    public String isFast() {
+        return "";
+    }
+
+    @Override
+    public String color() {
+        return null;
+    }
+
+    public String Ugly() {
+        return "yes";
+    }
+
+    public  String stash(){return "hi";}
+
+    public  String stash1(){return "hi";}
+
+    public  String stash2(){return "hi";}
+
+}
