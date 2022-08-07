513ecad05f30974091d2a2764b13468c5a7b2aaf
Yoana Slavcheva <Yoana.Slavcheva@mail.schwarz>
2022-08-07 21:16:48 +0300

    added eat

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Cat tom = new Cat();
         tom.setNoOfLegs(4);
+        tom.setVegetarian(false);
         tom.setName("Tommy");
+        tom.setEats("fish");
 
     }
 }
